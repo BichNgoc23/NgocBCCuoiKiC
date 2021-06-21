cd172fbf0e79d29716bf22a8562caca0c651506e
BichNgoc23 <86187485+BichNgoc23@users.noreply.github.com>
2021-06-21 11:19:17 +0700

    Add files via upload

---
+++ Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,123 @@
+﻿using ModelEF.Dao;
+using ModelEF.EF;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestUngDung.Areas.Admin.Controllers
+{
+    public class ProductController : BaseController
+    {
+        // GET: Admin/Product
+        NguyenThiBichNgocContext db = new NguyenThiBichNgocContext();
+        public ActionResult Index(string searchString)
+        {
+            var product = new ProductDao();
+            var model = product.ListWhereAll(searchString);
+            ViewBag.searchString = searchString;
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            setViewBag();
+            return View();
+        }
+        public void setViewBag(int? selectedID = null)
+        {
+            var dao = new CategoryDao();
+            ViewBag.IDCategory = new SelectList(dao.ListAll(), "ID", "Name", selectedID);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product sp, HttpPostedFileBase Image)
+        {
+
+
+            if (Image != null && Image.ContentLength > 0)
+            {
+               
+                string _FileName = Path.GetFileName(Image.FileName);
+                string _path = Path.Combine(Server.MapPath("~/Upload/sanpham/" + _FileName));
+                Image.SaveAs(_path);
+
+                sp.Image = _FileName;
+
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(sp);
+                db.SaveChanges();
+                SetAlert("Thêm Sản Phẩm Thành Công", "success");
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(System.Int32 id)
+        {
+            var product = new ProductDao();
+            var result = product.FindId(id);
+            setViewBag(result.ID);
+            if (result != null)
+                return View(result);
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product sp, HttpPostedFileBase Image)
+        {
+            Product unv = db.Products.FirstOrDefault(x => x.ID == sp.ID);
+
+            if (unv != null)
+            {
+                unv.IDCategory = sp.IDCategory;
+                unv.Name = sp.Name;
+                unv.UnitCost = sp.UnitCost;
+                unv.Quantity = sp.Quantity;
+                unv.Description = sp.Description;
+                unv.Status = sp.Status;
+                if (Image != null && Image.ContentLength > 0)
+                {
+                    int id = sp.ID;
+
+                    string _FileName = "";
+                    _FileName = Path.GetFileName(Image.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/Upload/sanpham/" + _FileName));
+                    Image.SaveAs(_path);
+                    unv.Image = _FileName;
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.SaveChanges();
+                SetAlert("Cập Nhật Sản Phẩm Thành Công", "success");
+                return RedirectToAction("Index");
+            }
+            setViewBag(sp.ID);
+            return View();
+        }
+       
+
+        public ActionResult Delete(int id)
+        {
+            var dao = new ProductDao().Delete(id);
+            if (dao)
+            {
+                SetAlert("Đã xóa thành công", "success");
+                return RedirectToAction("Index", "Product");
+            }
+            else
+            {
+                SetAlert("Xóa không thành công ", "error");
+                return RedirectToAction("Index", "Product");
+            }
+
+        }
+    }
+}
---
+++ ModelEF/Dao/ProductDao.cs
@@ -0,0 +1,77 @@
+﻿using ModelEF.EF;
+using ModelEF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelEF.Dao
+{
+    public class ProductDao
+    {
+        private NguyenThiBichNgocContext db;
+        public ProductDao()
+        {
+            db = new NguyenThiBichNgocContext();
+        }
+        public List<ProductModel> ListWhereAll(string keysearch)
+        {
+            var list_pro = from p in db.Products
+                               join c in db.Categories
+                               on p.IDCategory equals c.ID
+                               orderby p.Quantity, p.UnitCost descending
+                               select new ProductModel
+                               {
+                                   ID = p.ID,
+                                   Name = p.Name,
+                                   UnitCost = (decimal)p.UnitCost,
+                                   Quantity = p.Quantity,
+                                   Image = p.Image,
+                                   Description = p.Description,
+                                   category_name=c.Name
+                               };
+            var list_pro_1 = from p in db.Products
+                                 join c in db.Categories
+                                 on p.IDCategory equals c.ID
+                                 where p.Name.Contains(keysearch)
+                                 orderby p.Quantity, p.UnitCost descending
+                                 select new ProductModel 
+                                 {
+                                     ID = p.ID,
+                                     Name = p.Name,
+                                     UnitCost = (decimal)p.UnitCost,
+                                     Quantity = p.Quantity,
+                                     Image = p.Image,
+                                     Description = p.Description,
+                                     category_name = c.Name
+                                 };
+            if (!string.IsNullOrEmpty(keysearch))
+                return list_pro_1.ToList();
+            return list_pro.ToList();
+        }
+        public List<Product> ListAll()
+        {
+            return db.Products.Where(x => x.Status == 1).ToList();
+        }
+        public Product FindId(System.Int32 id)
+        {
+            return db.Products.Find(id);
+        }
+        public bool Delete(System.Int32 id)
+        {
+            try
+            {
+                var product = db.Products.Find(id);
+                db.Products.Remove(product);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+    }
+}
